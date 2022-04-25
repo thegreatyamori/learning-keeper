4fece3afb2ac822bce85f8ab079e945302d7ab7a
thegreatyamori <tod.jerson.morocho@gmail.com>
2022-04-24 23:08:34 -0500

    style: configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,37 @@
 module.exports = {
-  content: [],
+  mode: "jit",
+  purge: ["./src/**/*.{html,svg,js}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      display: ["Share", "system-ui"],
+    },
+    fontSize: {
+      base: "14px",
+    },
+    extend: {
+      colors: {
+        primary: {
+          light: "#FFFFFF",
+          DEFAULT: "#FFFFFF",
+          dark: "#1D1B1B",
+        },
+        secondary: {
+          light: "#000000",
+          DEFAULT: "#000000",
+          dark: "#51BA2C",
+        },
+        tertiary: {
+          "light-25": "#9A9A9A",
+          light: "#E1E1E1",
+          DEFAULT: "#E1E1E1",
+          dark: "#000000",
+          "dark-25": "#808080",
+        },
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
-}
+};
