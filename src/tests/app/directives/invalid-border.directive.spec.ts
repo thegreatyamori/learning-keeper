1e78ba0cbf0ee899c7ad40a847a36b86017f293c
thegreatyamori <tod.jerson.morocho@gmail.com>
2022-05-11 22:23:06 -0500

    feat: add invalid border directive

---
+++ src/tests/app/directives/invalid-border.directive.spec.ts
@@ -0,0 +1,8 @@
+import { InvalidBorderDirective } from '../../../app/directives/invalid-border.directive';
+
+describe('InvalidBorderDirective', () => {
+  it('should create an instance', () => {
+    const directive = new InvalidBorderDirective();
+    expect(directive).toBeTruthy();
+  });
+});
