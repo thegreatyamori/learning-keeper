596f502cae05d119d0364d98e8fe6e12f6420b2b
thegreatyamori <tod.jerson.morocho@gmail.com>
2022-05-11 22:21:53 -0500

    feat: add textarea logic

--- src/app/components/title-textarea/title-textarea.component.ts
+++ src/app/components/title-textarea/title-textarea.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class TitleTextareaComponent implements OnInit {
+  title: string = ''
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  handleValue(evt: Event): void {
+    let sentence = this.retrieveValue(evt)
+    let isValidSentence = this.validateSentence(sentence)
+
+    console.log(sentence, isValidSentence);
+    if (!isValidSentence) throw "Not valid text !";
+
+    this.title = sentence;
   }
 
+  private retrieveValue(evt: Event): string {
+    const target = evt.target as HTMLTextAreaElement;
+    return target.value;
+  }
+
+  private validateSentence(payload: string): boolean {
+    const validate_sentence_regex = new RegExp(/([^\.\?\!]*)[\.\?\!]/, 'g')
+    return validate_sentence_regex.test(payload)
+  }
 }
