b901fa157aa4e9cfe5e35dbdad124bcffe4ae85f
thegreatyamori <tod.jerson.morocho@gmail.com>
2022-05-11 22:22:28 -0500

    test: add some test cases for component

--- src/tests/app/components/title-textarea/title-textarea.component.spec.ts
+++ src/tests/app/components/title-textarea/title-textarea.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { TitleTextareaComponent } from 'src/app/components/title-textarea/title-textarea.component';
 
@@ -8,9 +9,9 @@ describe('TitleTextareaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TitleTextareaComponent ]
+      declarations: [TitleTextareaComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +22,39 @@ describe('TitleTextareaComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should have title as name attribute", () => {
+    const ngContainer = fixture.nativeElement as HTMLElement;
+    const textarea = ngContainer.firstElementChild as HTMLTextAreaElement;
+
+    expect(textarea.name).toEqual('title');
+  });
+
+  it("should have 'hello world.' as title value", () => {
+    const textarea = fixture.debugElement.query(By.css('.textarea-title')).nativeElement;
+    const event = new Event('change');
+    textarea.value = 'hello world.';
+    textarea.dispatchEvent(event);
+
+    fixture.detectChanges()
+
+    fixture.whenStable().then(() => {
+      expect(component.title).toEqual('hello world.');
+    })
+  });
+
+  it("should have error: 'Not valid text !' when have 'hello world' value", () => {
+    const textarea = fixture.debugElement.query(By.css('.textarea-title')).nativeElement;
+    const event = new Event('change');
+    textarea.value = 'hello world';
+    textarea.dispatchEvent(event);
+
+    fixture.detectChanges()
+
+    fixture.whenStable().then(() => {
+      expect(component.title).toEqual('');
+      expect(component.handleValue(event)).toThrowError('Not valid text !')
+    })
   });
 });
